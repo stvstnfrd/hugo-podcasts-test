daca2fc55cd4d0e979fe5e91fecfd90d9fab66c5
stvstnfrd <stvstnfrd@noreply.users.github.com>
2023-03-19 23:03:33 -0600

    fixup! Merge branch 'feat/create-rss-theme' into um

--- lib/make/hugo.mk
+++ lib/make/hugo.mk
@@ -1,7 +1,7 @@
 HUGO=hugo
 HUGO_FLAGS=--buildDrafts --cacheDir=$(PWD)/tmp/cache
 ifneq (,$(GITHUB_REPOSITORY))
-HUGO_FLAGS+=--baseURL 'https://$(GITHUB_USER_NAME).github.io/$(GITHUB_REPO_NAME)'
+HUGO_FLAGS+=--baseURL 'https://$(GITHUB_USER_NAME).github.io/$(GITHUB_REPO_NAME)/'
 endif
 HUGO_SITE_NAME=src
 HUGO_THEME=$(shell $(PYTHON) ./bin/get-key-from-yaml.py $(HUGO_SITE_NAME)/config.yaml theme)
